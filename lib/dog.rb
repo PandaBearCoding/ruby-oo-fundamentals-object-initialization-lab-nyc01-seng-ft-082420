314f73ff17b06395331bca82d073a79b393f5a32
pandabearcoding <madelinerose16@outlook.com>
2021-02-17 11:24:22 -0500

    week 6, commit 4: Going Through Bootcamp Material From Beginning

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+    # initialize
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+    end
+
+    # # setter 
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # # getter
+    # def name
+    #     @name
+    # end
+
+    #     # setter 
+    #     def breed=(breed)
+    #         @breed = breed
+    #     end
+    
+    #     # getter
+    #     def breed
+    #         @breed
+    #     end
+end 
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person 
+    # initialize
+    def initialize(name)
+        @name = name
+    end
+    
+    # # setter
+    # def name=(name)
+    #     @name = name
+    # end 
+    
+    # # getter
+    # def name
+    #     @name
+    # end
+end 
